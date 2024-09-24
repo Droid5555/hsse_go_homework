df7b898279f5cd57206d29b52e2bb0a011521cbe
droidbook <david.aleksian@phystech.edu>
2024-09-24 19:20:23 +0300

    added ability to set non-empty container as storage & code style

--- task1/task1.go
+++ task1/task1.go
@@ -19,6 +19,8 @@ type PairIdBook struct {
 	book Book
 }
 
+type idGenerator func(Book) string
+
 type BookSlice []PairIdBook
 type BookMap map[string]Book
 
@@ -30,39 +32,39 @@ type StorageInterface interface {
 type LibraryInterface interface {
 	Search(name string) (Book, bool)
 	Add(book Book) bool
-	SetIdGenerator(func(Book) string)
+	SetIdGenerator(idGenerator)
 	SetStorage(StorageInterface)
 }
 
 type Library struct {
 	storage  StorageInterface
 	nameToId map[string]string
-	hashFunc func(Book) string
+	hashFunc idGenerator
 }
 
 //Storage functions
 
-func (book_slice BookSlice) Search(id string) (Book, bool) {
-	for _, pair_id_book := range book_slice {
-		if pair_id_book.id == id {
-			return pair_id_book.book, true
+func (bookSlice BookSlice) Search(id string) (Book, bool) {
+	for _, pairIdBook := range bookSlice {
+		if pairIdBook.id == id {
+			return pairIdBook.book, true
 		}
 	}
 	return Book{}, false
 }
 
-func (book_map BookMap) Search(id string) (Book, bool) {
-	b, ok := book_map[id]
+func (bookMap BookMap) Search(id string) (Book, bool) {
+	b, ok := bookMap[id]
 	return b, ok
 }
 
-func (book_slice BookSlice) Add(id string, book Book) (StorageInterface, bool) {
-	return append(book_slice, PairIdBook{id, book}), true
+func (bookSlice BookSlice) Add(id string, book Book) (StorageInterface, bool) {
+	return append(bookSlice, PairIdBook{id, book}), true
 }
 
-func (book_map BookMap) Add(id string, book Book) (StorageInterface, bool) {
-	book_map[id] = book
-	return book_map, true
+func (bookMap BookMap) Add(id string, book Book) (StorageInterface, bool) {
+	bookMap[id] = book
+	return bookMap, true
 }
 
 // Library functions
@@ -86,14 +88,15 @@ func (lib *Library) Add(book Book) bool {
 		lib.nameToId[book.title] = id
 		return ok
 	}
-	fmt.Println("Can't add the book: ", book)
+	log.Println("Can't add the book: ", book)
 	return ok
 }
 
-func (lib *Library) SetIdGenerator(function func(Book) string) {
+func (lib *Library) SetIdGenerator(function idGenerator) {
 	if function == nil {
 		return
 	}
+
 	if lib.hashFunc == nil {
 		lib.hashFunc = function
 		return
@@ -112,10 +115,10 @@ func (lib *Library) SetIdGenerator(function func(Book) string) {
 	for name, prevId := range lib.nameToId {
 		book, ok := lib.storage.Search(lib.nameToId[name])
 		if ok {
-			newId = lib.hashFunc(book)
+			newId = function(book)
 			newLibStorage, _ = newLibStorage.Add(newId, book)
 		} else {
-			fmt.Println("Can't find the book at the id: ", prevId, " while transferring books to new IDs")
+			log.Println("Can't find the book at the id: ", prevId, " while transferring books to new IDs")
 		}
 		lib.nameToId[name] = newId
 	}
@@ -133,39 +136,58 @@ func (lib *Library) SetStorage(container StorageInterface) {
 		lib.storage = container
 	}
 
+	var newLibStorage StorageInterface
+
 	switch container.(type) {
 	case BookSlice:
-		container = make(BookSlice, 0)
+		newLibStorage = make(BookSlice, 0)
 	case BookMap:
-		container = make(BookMap)
+		newLibStorage = make(BookMap)
 	}
 
 	for name, id := range lib.nameToId {
 		book, ok := lib.storage.Search(lib.nameToId[name])
 		if ok {
-			container, _ = container.Add(id, book)
+			newLibStorage, _ = newLibStorage.Add(id, book)
 		} else {
-			fmt.Println("Can't find the book at the id: ", id, " while transferring books to the new container")
+			log.Println("Can't find the book at the id: ", id, " while transferring books to the new container")
 		}
 	}
 
-	lib.storage = container
+	// Transferring elements from the container (it may not be empty)
+	switch container.(type) {
+	case BookSlice:
+		for _, pairIdBook := range container.(BookSlice) {
+			book := pairIdBook.book
+			id := lib.hashFunc(book)
+			lib.nameToId[book.title] = id
+			newLibStorage, _ = newLibStorage.Add(id, book)
+		}
+	case BookMap:
+		for _, book := range container.(BookMap) {
+			id := lib.hashFunc(book)
+			lib.nameToId[book.title] = id
+			newLibStorage, _ = newLibStorage.Add(id, book)
+		}
+	}
+	lib.storage = newLibStorage
 }
 
 func Hash1(b Book) string {
 	data := b.title + b.author
 	hash := sha256.Sum256([]byte(data))
-	return hex.EncodeToString(hash[:])
+	return hex.EncodeToString(hash[:]) + "HASH1"
 }
+
 func Hash2(b Book) string {
 	data := b.title + b.author
 	hash := sha256.Sum256([]byte(data))
 	hashInt := int(binary.BigEndian.Uint64(hash[:8]))
-	return strconv.Itoa(hashInt)
+	return strconv.Itoa(hashInt) + "HASH2"
 }
 
 func main() {
-	fmt.Println("TEST 1")
+	fmt.Println("\nTEST 1")
 	// Создать слайс книг и библиотеку
 	storage := []Book{{"Анна Каренина", "Лев Толстой"}, {"1984", "George Orwell"}}
 	var lib LibraryInterface = &Library{}
@@ -179,7 +201,7 @@ func main() {
 	fmt.Println(lib.Search("Анна Каренина"))
 	fmt.Println(lib.Search("1984"))
 
-	fmt.Println("TEST 2")
+	fmt.Println("\nTEST 2")
 	// Заменить функцию генератор id
 	lib.SetIdGenerator(Hash2)
 	// найти еще книгу в библиотеке
@@ -189,7 +211,7 @@ func main() {
 	fmt.Println(lib.Search("ПАЮЩиЕ В ТЕРНОВНИКЕ"))
 	fmt.Println(lib.Search("Поющие в терновнике"))
 
-	fmt.Println("TEST 3")
+	fmt.Println("\nTEST 3")
 	// Заменить хранилище
 	lib.SetStorage(BookSlice{})
 	// Заполнить библиотеку
@@ -197,10 +219,22 @@ func main() {
 	lib.Add(Book{"Гарри Поттер и Орден Феникса", "Джоан Роулинг"})
 	lib.Add(Book{"Математический анализ, интегралы и ряды", "Тюленев Александр"})
 	lib.Add(Book{"How to be the best actor in the whole wide world", "Ryan Gosling"})
-	// Найти 1-2 книги в ней.
+
 	fmt.Println(lib.Search("Анна Каренина"))
 	fmt.Println(lib.Search("Гарри Поттер и Узник Азкабана"))
 	fmt.Println(lib.Search("Гарри Поттер и Орден Феникса"))
 	fmt.Println(lib.Search("Детство. Отрочество. Юность."))
 	fmt.Println(lib.Search("Ryan Gosling"))
+
+	fmt.Println("\nTEST 4 (non-empty container)")
+
+	lib.SetStorage(BookMap{
+		"1": Book{"Язык программирования С++", "Бьёрн Страуструп"},
+		"2": Book{"Правила и основы игры го", "Джон Фейрбейрн"},
+	})
+
+	fmt.Println(lib.Search("Язык программирования С++"))
+	fmt.Println(lib.Search("Правила и основы игры го"))
+	fmt.Println(lib.Search("Анна Каренина"))
+	fmt.Println(lib.Search("Детство. Отрочество. Юность."))
 }
